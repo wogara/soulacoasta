7a85dee42baa28b3fff54036964d79736a43e860
wogara <weogara@gmail.com>
2024-06-25 21:22:15 -0400

    created 'coming soon' shop modal

--- components/main-footer/main-footer.js
+++ components/main-footer/main-footer.js
@@ -3,35 +3,48 @@
 import { useState } from 'react';
 import ConnectModal from '../modals/connect-modal';
 import MusicPlayer from '../music-player/music-player';
+import ShopModal from '../modals/shop-modal';
 
 export default function ButtonGroup() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isConnectModalOpen, setIsConnectModalOpen] = useState(false);
+  const [isShopModalOpen, setIsShopModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
+  const openConnectModal = () => {
+    setIsConnectModalOpen(true);
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const closeConnectModal = () => {
+    setIsConnectModalOpen(false);
+  };
+  const openShopModal = () => {
+    setIsShopModalOpen(true);
+  };
+
+  const closeShopModal = () => {
+    setIsShopModalOpen(false);
   };
 
   return (
     <div>
       <div className="bg-black border-b-2 border-white p-4 fixed bottom-0 left-0 w-full z-50">
         <div className="flex justify-between items-center">
-          <button className="text-white hover:text-gray-400 p-2 border border-white h-12 w-24 flex items-center justify-center">
+          <button
+            onClick={openShopModal}
+            className="text-white hover:text-gray-400 p-2 border border-white h-12 w-24 flex items-center justify-center"
+          >
             Shop
           </button>
           <MusicPlayer />
           <button
-            onClick={openModal}
+            onClick={openConnectModal}
             className="text-white hover:text-gray-400 p-2 border border-white h-12 w-24 flex items-center justify-center"
           >
             Connect
           </button>
         </div>
       </div>
-      <ConnectModal isOpen={isModalOpen} onClose={closeModal} />
+      <ConnectModal isOpen={isConnectModalOpen} onClose={closeConnectModal} />
+      <ShopModal isOpen={isShopModalOpen} onClose={closeShopModal} />
     </div>
   );
 }
