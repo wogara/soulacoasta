6bc3bb5efe7aea7793d8da4f8bf8d417f01feeb4
wogara <weogara@gmail.com>
2024-06-29 14:18:43 -0400

    fixed modal

--- components/modals/show-modal.js
+++ components/modals/show-modal.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useRef } from 'react';
 
-export default function ShowModal({ isOpen, onClose,show }) {
+export default function ShowModal({ isOpen, onClose, show }) {
   const modalRef = useRef(null);
 
   useEffect(() => {
@@ -29,7 +29,7 @@ export default function ShowModal({ isOpen, onClose,show }) {
     <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
       <div
         ref={modalRef}
-        className="relative border border-white bg-black p-8 rounded shadow-lg w-60 h-60 flex justify-center items-center"
+        className="relative border border-white bg-black p-8 rounded shadow-lg flex justify-center items-center w-auto max-w-2xl"
       >
         <button
           onClick={onClose}
@@ -37,7 +37,35 @@ export default function ShowModal({ isOpen, onClose,show }) {
         >
           X
         </button>
-        <h1 className="text-2xl text-center">Coming Soon {show.flyer}</h1>
+        <img src={show.flyer} alt="Flyer" className="w-1/2 h-auto rounded" />
+        <div className="ml-8">
+          <h1 className="text-2xl text-center mb-4">{show.title}</h1>
+          <p className="text-white mb-2">
+            <strong>Date:</strong> {new Date(show.date).toLocaleDateString()}
+          </p>
+          <p className="text-white mb-2">
+            <strong>City:</strong> {show.city}
+          </p>
+          <p className="text-white mb-2">
+            <strong>Venue:</strong> {show.venue}
+          </p>
+          <p className="text-white mb-2">
+            <strong>Address:</strong> {show.address}
+          </p>
+          {show.link !== 'N/A' && (
+            <p className="text-white mb-2">
+              <strong>Link:</strong>{' '}
+              <a
+                href={show.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline hover:text-gray-400"
+              >
+                Here
+              </a>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
